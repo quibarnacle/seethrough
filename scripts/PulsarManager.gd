0e978913fb2242faf2311e6f6852b7bdf0e35b43
quibarnacle <quibarnacle@protonmail.com>
2021-03-02 10:28:34 +0100

    Building blocks for level B

--- scripts/PulsarManager.gd
+++ scripts/PulsarManager.gd
@@ -5,19 +5,26 @@ signal pulsar_group_desactivated
 
 var _nb_expected : int
 var _nb_active := 0
+var _push_up_on_activated := []
 
 func _ready():
-	_nb_expected = get_child_count()
+	_nb_expected = 0
 	for child in get_children():
-		child.connect("activated", self, "_on_Pulsar_activated")
-		child.connect("desactivated", self, "_on_Pulsar_desactivated")
-		self.connect("pulsar_group_activated", child, "_on_PulsarManagar_pulsar_group_activated")
-		self.connect("pulsar_group_desactivated", child, "_on_PulsarManagar_pulsar_group_desactivated")
+		if child.is_in_group("pulsar_switchs"):
+			_nb_expected += 1
+			child.connect("activated", self, "_on_Pulsar_activated")
+			child.connect("desactivated", self, "_on_Pulsar_desactivated")
+			self.connect("pulsar_group_activated", child, "_on_PulsarManagar_pulsar_group_activated")
+			self.connect("pulsar_group_desactivated", child, "_on_PulsarManagar_pulsar_group_desactivated")
+		elif child.is_in_group("pulsar_targets"):
+			_push_up_on_activated.push_back(child)
 		
 func _on_Pulsar_activated():
 	_nb_active += 1
 	if _nb_active == _nb_expected:
-		emit_signal("pulsar_group_activated")
+		for push_up in _push_up_on_activated:
+			push_up.z_index = 2
+			emit_signal("pulsar_group_activated")
 
 func _on_Pulsar_desactivated():
 	if _nb_active == _nb_expected:
